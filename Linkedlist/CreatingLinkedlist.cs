ace92df2c3fc2661449e33032a52adce4bd0f277
puja <pujarana1796@gmail.com>
2022-05-10 16:05:58 +0530

    puja added first commit

---
+++ Linkedlist/CreatingLinkedlist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist
+{
+    public class CreatingLinkedlist
+    {
+       internal Node head;
+
+        public void InsertFront(int data)
+        {
+            Node new_node = new Node(data);
+            new_node.next = head;
+            head = new_node;
+        }
+        public void display()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
--- Linkedlist/Program.cs
+++ Linkedlist/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("welcome to linkedlist programming");
+using System;
+namespace Linkedlist
+{
+    class program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to linkedlist programming");
+            CreatingLinkedlist linkedlist = new CreatingLinkedlist();
+            linkedlist.head = new Node(70);
+            Node second = new Node(30);
+            Node third = new Node(56);
+            linkedlist.head.next = second;
+            second.next = third;
+            Console.WriteLine("inserted elements");
+            linkedlist.display();
+        }
+    }
+}
